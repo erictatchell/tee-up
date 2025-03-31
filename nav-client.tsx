fe37d3b5a94b1ff71a2dfefb0be42d42a0f57edb
derekd03 <134988441+derekd03@users.noreply.github.com>
2025-03-31 15:28:29 -0700

    Update nav-client.tsx

--- nav-client.tsx
+++ nav-client.tsx
@@ -1,42 +1,42 @@
 
-"use client"; 
+"use client";
 
 import { signIn, signOut } from "next-auth/react";
 
-import MButton from "./app/components/misc/button";
+import MButton from "./app/components/misc/Button";
 interface NavClientProps {
   name?: string;
 }
 
 const NavClient: React.FC<NavClientProps> = ({ name }) => {
   return (
-			<>
-        {!name ? (
-          <button
-            type="submit"
-            className="p-2 bg-blue-700 w-[60%] mx-auto rounded-md hover:bg-blue-800"
-            onClick={() =>
-              signIn("google", {
-                callbackUrl: "/profile",
-              })
-            }
-          >
-            Login with Google
-          </button>
-        ) : (
-          <button
-            type="submit"
-            className="p-2 bg-blue-700 w-[60%] mx-auto rounded-md hover:bg-blue-800"
-            onClick={() =>
-              signOut({
-                callbackUrl: "/",
-              })
-            }
-          >
-            Sign out
-          </button>
-        )}
-				</>
+    <>
+      {!name ? (
+        <button
+          type="submit"
+          className="p-2 bg-blue-700 w-[60%] mx-auto rounded-md hover:bg-blue-800"
+          onClick={() =>
+            signIn("google", {
+              callbackUrl: "/profile",
+            })
+          }
+        >
+          Login with Google
+        </button>
+      ) : (
+        <button
+          type="submit"
+          className="p-2 bg-blue-700 w-[60%] mx-auto rounded-md hover:bg-blue-800"
+          onClick={() =>
+            signOut({
+              callbackUrl: "/",
+            })
+          }
+        >
+          Sign out
+        </button>
+      )}
+    </>
   );
 };
 
