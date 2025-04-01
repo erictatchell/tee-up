b01c04a8350fb79dc60f2c75feff97bb0bebb5cc
erictatchell <etatchell@outlook.com>
2025-04-01 14:38:55 -0700

    auth checking in home for conditional sign in button viewing

--- app/profile/filters/page.tsx
+++ app/profile/filters/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState, useEffect } from "react";
 import { Range } from "react-range";
-import MButton from "@/app/components/misc/Button";
+import MButton from "@/app/components/misc/button";
 
 export default function ProfileFilters() {
 
